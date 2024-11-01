58529e16103b8425b39b77c60a9422b9ac07d4be
savallarian45@gmail.com <savallarian45@gmail.com>
2024-10-31 23:20:14 -0300

    agregado favicon

--- application/views/admin/listaUsuarios.php
+++ application/views/admin/listaUsuarios.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/styles.css'); ?>">
+    <link rel="icon" type="image/x-icon" href="<?php echo base_url('assets/Imagenes/logo.ico'); ?>">
+
     <title>Admin</title>
 </head>
 <body data-bs-theme="dark">
